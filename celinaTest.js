3638bf437d174a6126f1eda886c273b97a471bbe
Celina Söderman <celina.soderman@gmail.com>
2023-02-19 18:37:01 +0100

    Buttons to view all types added

--- celinaTest.js
+++ celinaTest.js
@@ -2,9 +2,15 @@ function main(){
     retriveData();
 }
 
-async function retriveData(){
-    var nameList = [];
-    var HP_list = [];
+var pokeInfo =[
+    ["name", "Type", "HP"]
+];
+
+async function retriveData(type){
+    var xData = [];
+    var yData = [];
+    var types = ["Grass","Poison","Fire","Flying","Water","Bug","Normal","Electric","Ground","Fairy","Fighting","Psychic","Rock","Steel","Ice","Ghost","Dragon"];
+    
 
 //Fetching data and storing data
     for(var i = 1; i <= 151; i++){
@@ -14,20 +20,26 @@ async function retriveData(){
 
         var tempName = JSON.stringify(pokemon.name).split("\"")[1];
         var tempHP = parseInt(JSON.stringify(pokemon.stats[0]).split(",")[0].split(":")[1]);
+        var tempType = JSON.stringify(pokemon.types).split("name")[1].split(",")[0].split("\"")[2];
 
-        HP_list.push(tempHP);
-        nameList.push(tempName);
+        //console.log(tempType);
+        yData.push(tempHP);
+        xData.push(tempName);
+
+        var tempInfo = [tempName,tempType, tempHP];
+        pokeInfo.push(tempInfo); 
+        
     }
-    buildChart(HP_list,nameList);
+    sortData("grass");
 }
 
 // Building bar chart
-function buildChart(HP_list,nameList){
-    const matrix = [HP_list,nameList];
+function buildChart(yData,xData){
+    const matrix = [yData,xData];
     
     var margin = {top: 30, right: 30, bottom: 70, left: 60},
-        width = 1700 - margin.left - margin.right;
-        height = 1000  - margin.top - margin.bottom;
+        width = 1000 - margin.left - margin.right;
+        height = 750  - margin.top - margin.bottom;
 
 
     var svg = d3.select("#chart")
@@ -38,7 +50,7 @@ function buildChart(HP_list,nameList){
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
  
     var xScale = d3.scaleBand().range ([0, width]).padding(0.4);
-    xScale.domain(nameList);
+    xScale.domain(xData);
 
     svg.append("g")
         .attr("transform", "translate(0," + height  + ")")
@@ -50,23 +62,43 @@ function buildChart(HP_list,nameList){
     var yScale = d3.scaleLinear().range ([height,0]);
  
    
-    yScale.domain([0, d3.max(HP_list)]);
+    yScale.domain([0, d3.max(yData)]);
 
     svg.append("g")
     .call(d3.axisLeft(yScale).tickFormat(function(d){
         return d;
-    }).ticks(d3.max(HP_list)/10));
+    }).ticks(d3.max(yData)/10));
 
-    for (var i = 0; i < nameList.length ;i++){
+    for (var i = 0; i < xData.length ;i++){
         svg.selectAll(".bar")
         .data(matrix)
         .enter().append("rect")
-        .attr("x", xScale(nameList[i]))
-        .attr("y", yScale(HP_list[i]))
+        .attr("x", xScale(xData[i]))
+        .attr("y", yScale(yData[i]))
         .attr("width", xScale.bandwidth())
-        .attr("height", height-yScale(HP_list[i]))
+        .attr("height", height-yScale(yData[i]))
         .attr("fill", "steelblue")
         ;        
     }
+}
+
+function sortData(type){
+    try{
+        var svg = d3.select("svg").remove();
+        svg.selectAll("*").remove();
+    }catch(e){
+
+    }
+    var xData = [];
+    var yData = [];
+    //console.log(pokeInfo);
+    for(var i = 1; i<152;i++){
+        if(pokeInfo[i][1] == type){
+
+            xData.push(pokeInfo[i][0]);
+            yData.push(pokeInfo[i][2]);
+        }
+    }
+    buildChart(yData,xData);
 }
 
